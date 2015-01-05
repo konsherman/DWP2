f203f40f5904f011b968e13ebb9a74759aa7a55c
Konner <konsherman@fullsail.edu>
2015-01-05 17:58:08 -0500

    madlib
    first comment setup and start

---
+++ MadLib/main.js
@@ -0,0 +1,39 @@
+
+
+
+// function evenNumber(a){
+//     var total = 0;
+//     for (var i =0; i < a.length; i++) {
+//         if(a[i]%2==0){
+//             total +=1;
+
+//     }
+// }
+// return total;
+// };
+// var evens = evenNumber([4,4,5,4,7])
+// console.log(evens);
+
+
+var name = prompt("Made up name"); //prompting user for a "made up name"
+var 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
