8b94a4c90e625052b3857f809ddb30aa3d07068e
Konner <konsherman@fullsail.edu>
2015-01-07 12:52:51 -0500

    done
    done

--- MadLib/main.js
+++ MadLib/main.js
@@ -46,12 +46,11 @@ var favName1 = prompt("Boy name");
 var favName2 = prompt("Girl Name");
 var favName3 = prompt("Cat name");
 
+var car = {color:color,year:year};
+
+
 var favNames = [favName1,favName2,favName3];
-
-
-
-
-
+console.log(name+" "+"Liked to eat lots of chicken nuggets, he was "+age+"Years old "+"Born in the year "+year+"His favorite color was"+color+"his favorite friends were"+"he had a "+car.year+car.color+"Car");
 //--------loop
 for(var i = 0; i<favNames.length;i++){
 	console.log(favNames[i]);
@@ -62,7 +61,8 @@ for(var i = 0; i<favNames.length;i++){
 
 
 // object 
-var car = {color:color,year:year};
+
+
 
 
 
