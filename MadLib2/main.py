2cf2c3d7e0f6054a821fb6c64e99037af2caa40f
Konner <konsherman@fullsail.edu>
2015-01-12 18:07:15 -0500

    comments
    updating comments

--- MadLib2/main.py
+++ MadLib2/main.py
@@ -1,23 +1,29 @@
-
-for i in range(5):
+#loop
+for i in range(5):#counting down
     print "COUNT DOWN"
     print i
+#end loop
 
 name1 = raw_input("First name")
 name2 = raw_input("Last name")
 
+
+#array---
 array = []
 
 array.append(name1)
 array.append(name2)
-
+#pushing into the array
 
 weight = raw_input("Weight")
 
+
+#if statement
 if weight > 200:
     print "OverWeight"
 else:
     print "Perfect"
+#end if
 
 
 def area(l, w):
@@ -38,7 +44,7 @@ selection = raw_input("Enter number 1 or 2")
 print name1, name2, "weighed",weight, "lbs. The area of his house was ",area(length, width),"he liked ",foods[selection]
 
 
-     
+
 
 
 
